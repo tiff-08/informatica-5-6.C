c7cad5843a384f95987d133f41d204a459051010
tiff-08 <tiffanynicsp@gmail.com>
2026-08-17 19:38:09 +0000

    rectangle

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+def main():
+    width = int(input("Enter the width of the rectangle: "))
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+
+    p = (5 * 2) + (width * 2)
+    print("Perimeter:", p)
+
+if __name__ == "__main__":
+        main()
