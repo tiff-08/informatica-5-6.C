0d6148bc454d79f186b85e7d82c632a351e60334
tiff-08 <tiffanynicsp@gmail.com>
2026-08-20 16:40:02 +0000

    update

---
+++ update-variable.py
@@ -0,0 +1,30 @@
+def main():
+    # Augmented Assignment Operator
+    money = 5
+    money = money + 10
+    print(money) #This will print 15
+
+    # Substraction Assignment Operator
+    minutes = 60
+    minutes = minutes - 25
+    print(minutes) # This will print 35
+
+    # Multiply Assignment Operator
+    text = "Im not Victor"
+    text *= 20
+    print(text)
+
+    #Division Assignment Operator
+    pizza = 8
+    people = int(input("Number of people at the pizza party: "))
+    pizza /= people
+    print(pizza)
+
+    # Modulus Assignment Operator
+    leftover = 10
+    leftover %= people
+    print(lestover)
+
+
+if __name__=="__main__":
+        main()
