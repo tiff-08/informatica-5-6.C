c378b46472f4ca8f20c6720fed6d77dad95aff2d
tiff-08 <tiffanynicsp@gmail.com>
2026-08-24 19:32:24 +0000

    password

---
+++ password.py
@@ -0,0 +1,11 @@
+def main():
+        password = "110626"
+        attempt = input("Enter password: ")
+
+        if attempt == password:
+            print("Correct password.")
+
+        print("See you later.")
+
+if __name__ == "__main__":
+        main()
