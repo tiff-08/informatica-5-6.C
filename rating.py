f79a6ae52c7300fa03caf75ad4e7b7abdbbf2b89
tiff-08 <tiffanynicsp@gmail.com>
2026-08-28 17:24:25 +0000

    rating

--- rating.py
+++ rating.py
@@ -1,6 +1,17 @@
 def main():
     print("Tiff n co")
-    rating = float(input("Rate the restaurant))
+    rating = float(input("Rate the restaurant 0 to 5: "))
+
+    if rating > 4.5:
+        print ("Perfection")
+    elif rating > 4:
+        print("Excellent")
+    elif rating > 3:
+        print("Good")
+    elif rating > 2:
+        print("Fair")
+    else:
+        print("Poor")
 
 if __name__=="__main__":
     main()
