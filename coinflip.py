8a969dddd4cabc1b18737770a77d7739152e27e7
tiff-08 <tiffanynicsp@gmail.com>
2026-09-01 16:28:51 +0000

    coinflip

---
+++ coinflip.py
@@ -0,0 +1,19 @@
+import random
+
+def main():
+    guess = int(input("Enter your guess, 1 for Heada, 2 for Tails: "))
+    coinflip = random.randint(1, 2)
+
+    if coinflip == 1:
+        print("Heads")
+    elif coinflip == 2:
+        print("Tails")
+
+    if coinflip == guess
+        print("Your guess")
+    elif coinflip != guess
+
+
+
+if __name__ == "__main__":
+        main()
