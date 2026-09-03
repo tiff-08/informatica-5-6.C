7b15be72bb42cbafbf88939a4d5dc2b8b87816e5
tiff-08 <tiffanynicsp@gmail.com>
2026-09-03 17:28:41 +0000

    guess

---
+++ guess-number.py
@@ -0,0 +1,13 @@
+import random
+def main():
+    name = input("Hello! What is your name? ")
+    print("Well,",name,"I am thinking of a number between 1 and 100. Take a guess.")
+    input("Take a guess")
+    number = random.randint(1, 100)
+
+
+    while number !=
+
+
+if __name__ == "__main__":
+        main()
