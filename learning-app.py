ac006e4f5722b3f7a9895acb7b6247cb021e3924
tiff-08 <tiffanynicsp@gmail.com>
2026-09-04 17:30:22 +0000

    learning

---
+++ learning-app.py
@@ -0,0 +1,17 @@
+import random
+def main():
+        print("Adding with Terminal")
+        number1 = random.randint(10, 99)
+        number2 = random.randint(10, 99)
+
+        print(f"what is {number1} + {number2}")
+
+        add = number1 + number2
+        if result != add:
+                print("Incorrect")
+                print(f"The answer was {add}")
+
+
+
+if __name__ == "__main__":
+        main()
