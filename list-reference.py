6a8b2800b0d8c314681edc5e7fe9fc1bc5433460
tiff-08 <tiffanynicsp@gmail.com>
2026-09-10 20:20:31 +0000

    list

---
+++ list-reference.py
@@ -0,0 +1,20 @@
+def main():
+
+        #sort
+    numbers = [1, 4, 5, 7, 9, 3]
+    numbers.sort()
+    print(numbers)
+
+    items = ["Lettuce", "Tomato", "Bread", "Jam", "Mayonaise"]
+    items.sort()
+    print(items)
+
+    numbers1 = [1, 4, 5, 7, 9, 3]
+    numbers1.sort(reverse=True)
+    print(numbers1)
+
+
+
+
+if __name__ == "__main__":
+    main()
