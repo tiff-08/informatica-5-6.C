67e884b053607baf77ad5ef9ab1f643473395749
tiff-08 <tiffanynicsp@gmail.com>
2026-09-17 20:18:23 +0000

    triangle

--- for-intro.py
+++ for-intro.py
@@ -1,5 +1,9 @@
 def main():
-    faith repentence bautismo
+    doctrine = ["faith","repentance","baptism","confirmation","endure to the end"]
 
+    for principle in doctrine:
+        print(principle)
+    for i in range(len(doctrine)):
+        print(f"{i+1}. {doctrine[i]}")
 if __name__=="__main__":
     main()
---
+++ triangle.py
@@ -0,0 +1,7 @@
+def main():
+    height = int(input("Height: "))
+    for i in range(height):
+        print("*"*(i+1))
+
+if __name__=="__main__":
+    main()
