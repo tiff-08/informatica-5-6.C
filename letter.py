53bb45850fa491e6d2e88604d905ab80ce8fcfbb
tiff-08 <tiffanynicsp@gmail.com>
2026-09-23 19:55:32 +0000

    gb

--- letter.py
+++ letter.py
@@ -2,17 +2,17 @@ def main():
     friends = ["Mario","Luigi","Daisy","Yoshi","Toad","Princess Peach","Bowser"]
     for receiver in friends:
         if receiver != "Princess Peach":
-        print(f"""
-+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-    Dear {receiver},
+            print(f"""
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+        Dear {receiver},
 
-    You are cordially invited to a ball at
-    Peach's Castle this evening, 7:00 PM.
+        You are cordially invited to a ball at
+        Peach's Castle this evening, 7:00 PM.
 
-    Sincerely,
-    {friends[5]}
-+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-        """)
+        Sincerely,
+        {friends[5]}
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+            """)
 
 
 if __name__=="__main__":
