6203bd55a83cb0257b63dc76235d7d9fe444c304
tiff-08 <tiffanynicsp@gmail.com>
2026-09-23 20:20:19 +0000

    tables

--- tables.py
+++ tables.py
@@ -1,4 +1,11 @@
 def main():
-    
+    while True :
+        number = int(input("Enter a number from 1 to 10: "))
+
+        print(f"Here is the {number} times table")
+
+        if number() ==
+
+
 if __name__=="__main__":
     main()
